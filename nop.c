08c5606462cf3d6128b10b78b734e376d7779b67
myfreeer <myfreeer@gmail.com>
2016-06-01 13:35:02 +0000

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,41 +1,5 @@
-//+---------------------------------------------------------------------------
-//
-//  HELLO_WIN.C - Windows GUI 'Hello World!' Example
-//
-//+---------------------------------------------------------------------------
-
 #include <windows.h>
 
-#define APPNAME "HELLO_WIN"
-
-char szAppName[] = APPNAME; // The name of this application
-char szTitle[]   = APPNAME; // The title bar text
-const char *pWindowText;
-
-void CenterWindow(HWND hWnd);
-
-//+---------------------------------------------------------------------------
-//
-//  Function:   WndProc
-//
-//  Synopsis:   very unusual type of function - gets called by system to
-//              process windows messages.
-//
-//  Arguments:  same as always.
-//----------------------------------------------------------------------------
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    return 0;
-}
-
-//+---------------------------------------------------------------------------
-//
-//  Function:   WinMain
-//
-//  Synopsis:   standard entrypoint for GUI Win32 apps
-//
-//----------------------------------------------------------------------------
 int WinMain(
         HINSTANCE hInstance,
         HINSTANCE hPrevInstance,
@@ -43,12 +7,5 @@ int WinMain(
         int nCmdShow
         )
 {
-
-    return 0;
+        return 0;
 }
-
-//+---------------------------------------------------------------------------
-
-//+---------------------------------------------------------------------------
-
-//+---------------------------------------------------------------------------
